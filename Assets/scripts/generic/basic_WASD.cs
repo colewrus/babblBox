2081bcb8baf7bb87426f75877247b045a831d708
colewrus <wrampelc@gmail.com>
2017-03-13 08:01:31 -0400

    three bubble dialogue started. Need to create quick system for conversation

---
+++ Assets/scripts/generic/basic_WASD.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class basic_WASD : MonoBehaviour {
+
+	Vector3 move;
+	public List<string> animationNames = new List<string>();
+
+	public float speed;
+	float horizontal;
+	float vertical;
+
+	Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>(); //!------ NEEDS A RIGIDBODY 2D -------!
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+		AnimationSelector ();
+	}
+
+	void Movement(){
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
+
+		if (horizontal > 0)
+		{
+			move.x = 0.75f;
+			move.y = 0;
+		}
+		else if (horizontal < 0)
+		{
+			move.x = -0.75f;
+			move.y = 0;
+		}
+		else if (vertical > 0)
+		{
+			move.y = 0.75f;
+			move.x = 0;
+		}
+		else if (vertical < 0)
+		{
+			move.y = -0.75f;
+			move.x = 0;
+		}
+		else
+		{
+			move = Vector3.zero;
+		}
+
+		rb.velocity = new Vector2(move.x*speed, move.y*speed);
+	}
+
+
+	void AnimationSelector()
+	{
+		if (move.x < 0) //Left animation
+		{			
+			gameObject.GetComponent<Animator>().Play(animationNames[0]);
+		}
+		else if (move.x > 0) //right animation
+		{
+			gameObject.GetComponent<Animator>().Play(animationNames[1]);
+		}
+		else if (move.y > 0) //up animation
+		{
+			gameObject.GetComponent<Animator>().Play(animationNames[2]);
+		}
+		else if (move.y < 0) //down animation
+		{
+			gameObject.GetComponent<Animator>().Play(animationNames[3]);
+		}else  //idle animation
+		{
+			gameObject.GetComponent<Animator>().Play(animationNames[4]);
+		}
+	}
+}
---
+++ Assets/scripts/tri_bubble/mng_Bubble.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class mng_Bubble : MonoBehaviour {
+
+	bool startDialogue;
+	public List<GameObject> bubblesList = new List<GameObject>();
+	public List<string> bubble_String = new List<string> ();
+
+	// Attach this to the object that you want to 
+	void Start () {
+		startDialogue = false;
+		for (int i = 0; i < bubblesList.Count; i++) {
+			bubblesList [i].SetActive (false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnMouseDown(){ //make sure object has a box colider
+		print(gameObject.name);
+		RunDialogue ();
+
+	}
+
+
+	void RunDialogue(){
+		for (int i = 0; i < bubblesList.Count; i++) {
+			bubblesList [i].GetComponentInChildren<Text> ().text = bubble_String [i];
+			bubblesList [i].SetActive (true);
+		}
+	}
+}
